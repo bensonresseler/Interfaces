914b9506a6f0dfa1858327456170be032ae4e99f
bensonresseler <bresseler.vdab@gmail.com>
2018-08-27 11:18:01 +0200

    Oefn Interfaces - 03

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,59 +1,54 @@
 package com.company;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-            ArrayList<Persoon> personen = new ArrayList<>();
-            personen.add(new Persoon("Karen", LocalDate.of(1974, 10, 28)));
-            personen.add(new Persoon("Kristel", LocalDate.of(1975, 12, 10)));
-            personen.add(new Persoon("Kathleen", LocalDate.of(1978, 6, 18)));
+        List<Persoon> personen = new ArrayList<>();
+        personen.add(new Persoon("Karen",LocalDate.of(1974, 10, 28)));
+        personen.add(new Persoon("Kristel", LocalDate.of(1975, 12, 10)));
+        personen.add(new Persoon("Kathleen", LocalDate.of(1978, 6, 18)));
+        ToonGegevens(personen);
 
-        Collections.sort(personen);
-        System.out.println("Output: ");
-        for (Persoon p : personen) {
-            System.out.printf("%s(%d)%n", p.getNaam(), p.getLeeftijd());
+        List<Persoon> personen2 = new LinkedList<>();
+        personen2.add(new Persoon("Hanne", LocalDate.of(1994, 3, 3)));
+        personen2.add(new Persoon("Marthe", LocalDate.of(1996, 7, 16)));
+        personen2.add(new Persoon("Klaasje", LocalDate.of(1995, 3, 2)));
+        ToonGegevens(personen2);
+
+    }
+    private static void ToonGegevens(List<Persoon> personen){
+        for(Persoon p: personen){
+            System.out.printf("%s (%d jaar)%n", p.getNaam(), p.getLeeftijd());
         }
     }
 }
-
 class Persoon implements Comparable<Persoon> {
     private String naam;
-    private LocalDate geboortedatum;
-
-    public Persoon(String naam, LocalDate geboortedatum) {
+    private LocalDate geboorteDatum;
+    public Persoon(String naam, LocalDate geboorteDatum) {
         this.naam = naam;
-        this.geboortedatum = geboortedatum;
+        this.geboorteDatum = geboorteDatum;
     }
 
     public String getNaam() {
         return naam;
     }
 
-    public LocalDate getGeboortedatum() {
-        return geboortedatum;
+    public LocalDate getGeboorteDatum() {
+        return geboorteDatum;
     }
-
     public int getLeeftijd() {
         LocalDate vandaag = LocalDate.now();
-        int leeftijd = vandaag.getYear() - geboortedatum.getYear();
-        if (geboortedatum.getDayOfYear() > vandaag.getDayOfYear()) {
-            leeftijd--;
-        }
+        int leeftijd = vandaag.getYear() - geboorteDatum.getYear();
+        if (geboorteDatum.getDayOfYear() > vandaag.getDayOfYear()) leeftijd--;
         return leeftijd;
     }
 
     @Override
     public int compareTo(Persoon o) {
-        if (this.getLeeftijd() - o.getLeeftijd() < 0)
-            return -1;
-        if (this.getLeeftijd() - o.getLeeftijd() > 0)
-            return +1;
-        return 0;
+        return this.getLeeftijd() - o.getLeeftijd();
     }
 }
