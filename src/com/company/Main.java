64e71f9261d0e03cef9ed224fb2635455c3c7772
bensonresseler <bresseler.vdab@gmail.com>
2018-08-27 11:29:48 +0200

    Oefn Interfaces - 04, verwijder object uit Arraylist

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,28 +6,35 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        List<Persoon> personen = new ArrayList<>();
-        personen.add(new Persoon("Karen",LocalDate.of(1974, 10, 28)));
+        ArrayList<Persoon> personen = new ArrayList<>();
+        personen.add(new Persoon("Karen", LocalDate.of(1974, 10, 28)));
         personen.add(new Persoon("Kristel", LocalDate.of(1975, 12, 10)));
         personen.add(new Persoon("Kathleen", LocalDate.of(1978, 6, 18)));
-        ToonGegevens(personen);
-
-        List<Persoon> personen2 = new LinkedList<>();
-        personen2.add(new Persoon("Hanne", LocalDate.of(1994, 3, 3)));
-        personen2.add(new Persoon("Marthe", LocalDate.of(1996, 7, 16)));
-        personen2.add(new Persoon("Klaasje", LocalDate.of(1995, 3, 2)));
-        ToonGegevens(personen2);
-
-    }
-    private static void ToonGegevens(List<Persoon> personen){
-        for(Persoon p: personen){
+        personen.add(new Persoon("Hanne", LocalDate.of(1994, 3, 3)));
+        personen.add(new Persoon("Marthe", LocalDate.of(1996, 7, 16)));
+        personen.add(new Persoon("Klaasje", LocalDate.of(1995, 3, 2)));
+        FilterGegevens(personen);
+        for (Persoon p : personen) {
             System.out.printf("%s (%d jaar)%n", p.getNaam(), p.getLeeftijd());
         }
+
     }
+
+    private static void FilterGegevens(ArrayList<Persoon> personen) {
+        for (int i = 0; i < personen.size(); i++) {
+            if (personen.get(i).getLeeftijd() < 30) {
+                personen.remove(personen.get(i));
+                i--;
+            }
+        }
+    }
+
 }
+
 class Persoon implements Comparable<Persoon> {
     private String naam;
     private LocalDate geboorteDatum;
+
     public Persoon(String naam, LocalDate geboorteDatum) {
         this.naam = naam;
         this.geboorteDatum = geboorteDatum;
@@ -40,6 +47,7 @@ class Persoon implements Comparable<Persoon> {
     public LocalDate getGeboorteDatum() {
         return geboorteDatum;
     }
+
     public int getLeeftijd() {
         LocalDate vandaag = LocalDate.now();
         int leeftijd = vandaag.getYear() - geboorteDatum.getYear();
