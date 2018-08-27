0c07fcd68df7bd670c502adff06b639fa5bb73b7
bensonresseler <bresseler.vdab@gmail.com>
2018-08-27 12:25:43 +0200

    Oefn Interfaces - 07, extra vergelijking toevoegen via extra klasse -> 'Comparator'

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,70 +6,51 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         ArrayList<Persoon> personen = new ArrayList<>();
-        System.out.print("Geef naam (<RETURN> om te stoppen): ");
-        String naam =scanner.nextLine();
-        while(! "".equals(naam)){
-            System.out.print("Geef lengte: ");
-            double lengte = Double.parseDouble(scanner.nextLine());
-            System.out.print("Geef gewicht: ");
-            int gewicht = Integer.parseInt(scanner.nextLine());
-            personen.add(new Persoon(naam, lengte, gewicht));
-            System.out.print("Geef naam (<RETURN> om te stoppen): ");
-            naam =scanner.nextLine();
-        }
-        filterOpBMI(personen);
-        System.out.println("Personen met goede BMI:");
+        personen.add(new Persoon("Karen",LocalDate.of(1974, 10, 28)));
+        personen.add(new Persoon("Kristel", LocalDate.of(1975, 12, 10)));
+        personen.add(new Persoon("Kathleen", LocalDate.of(1978, 6, 18)));
+        personen.add(new Persoon("Hanne", LocalDate.of(1994, 3, 3)));
+        personen.add(new Persoon("Marthe", LocalDate.of(1996, 7, 16)));
+        personen.add(new Persoon("Klaasje", LocalDate.of(1995, 3, 2)));
+        personen.sort(new PersoonComparator());
         for(Persoon p: personen){
-            System.out.printf("%s (lengte:%.2f, gewicht: %d) heeft een BMI van %.2f%n", p.getNaam(), p.getLengte(), p.getGewicht(), p.getBMI());
-        }
-
-    }
-
-    private static void filterOpBMI(ArrayList<Persoon> personen) {
-        for (int i = 0; i < personen.size(); i++) {
-            if(personen.get(i).getBMI() < 18.5 || personen.get(i).getBMI() > 25){
-                personen.remove(personen.get(i));
-                i--;
-            }
+            System.out.println(p.getNaam());
         }
     }
 }
-class Persoon implements Comparable<Persoon>{
+class Persoon implements Comparable<Persoon> {
     private String naam;
-    private double lengte;
-    private int gewicht;
-
-    public Persoon(String naam, double lengte, int gewicht) {
+    private LocalDate geboorteDatum;
+    public Persoon(String naam, LocalDate geboorteDatum) {
         this.naam = naam;
-        this.lengte = lengte;
-        this.gewicht = gewicht;
+        this.geboorteDatum = geboorteDatum;
     }
 
     public String getNaam() {
         return naam;
     }
 
-    public double getLengte() {
-        return lengte;
+    public LocalDate getGeboorteDatum() {
+        return geboorteDatum;
     }
-
-    public int getGewicht() {
-        return gewicht;
-    }
-    public double getBMI(){
-        return gewicht / (lengte *lengte);
+    public int getLeeftijd() {
+        LocalDate vandaag = LocalDate.now();
+        int leeftijd = vandaag.getYear() - geboorteDatum.getYear();
+        if (geboorteDatum.getDayOfYear() > vandaag.getDayOfYear()) leeftijd--;
+        return leeftijd;
     }
 
     @Override
     public int compareTo(Persoon o) {
-        if (this.getBMI() < o.getBMI()){
-            return +1;
-        }else if (this.getBMI() > o.getBMI()){
-            return -1;
-        }else {
-            return 0;
-        }
+        return this.getLeeftijd() - o.getLeeftijd();
+    }
+}
+
+class PersoonComparator implements Comparator<Persoon>{
+
+    @Override
+    public int compare(Persoon p1, Persoon p2) {
+        return (p1.getNaam().compareTo(p2.getNaam()));
     }
 }
